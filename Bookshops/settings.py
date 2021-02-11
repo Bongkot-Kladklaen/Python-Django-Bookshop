c5c007111c4d02f5141d340658b6943edec9d8fc
Jaybong <bongkot.klad@gmail.com>
2021-02-11 22:50:50 +0700

    main(project): Deploy django to heroku

--- Bookshops/settings.py
+++ Bookshops/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-bookshop-basic.herokuapp.com']
+ALLOWED_HOSTS = ['django-bookshops-basic.herokuapp.com']
 
 
 # Application definition
